19add6a7794f77a47996aa23ed9590da78ce8fdb
NobleMahmud <mahmud.oneis1@gmail.com>
2023-12-23 14:32:53 +0600

    task added

--- index.js
+++ index.js
@@ -23,11 +23,59 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    const userCollection = client.db("taskDB").collection("users");
+    const taskCollection = client.db("taskDB").collection("tasks");
+
+     // user related api
+     app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    })
+    app.get('/users/:name', async (req, res) => {
+      const userName = req.params.name;
+      const query = {name: userName}
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    })
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: 'user already exists', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    })
+
+    //tasks related
+    app.get('/tasks', async(req, res)=>{
+      const result = await taskCollection.find().toArray();
+      res.send(result);
+    })
+
+    // app.delete('/users/:id', verifyToken, verifyAdmin, async (req, res) => {
+    //   const id = req.params.id;
+    //   const query = { _id: new ObjectId(id) }
+    //   const result = await userCollection.deleteOne(query)
+    //   res.send(result);
+    // })
+
+    // app.patch('/users/admin/:id', async (req, res) => {
+    //   const id = req.params.id;
+    //   const filter = { _id: new ObjectId(id) }
+    //   const updatedDoc = {
+    //     $set: {
+    //       role: 'admin'
+    //     }
+    //   }
+    //   const result = await userCollection.updateOne(filter, updatedDoc);
+    //   res.send(result);
+    // })
+
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
